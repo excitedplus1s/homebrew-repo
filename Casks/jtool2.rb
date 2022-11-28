40cc05dc2705a11ffcbd1a7112a3ee290cecad77
excitedplus1s <81917660+excitedplus1s@users.noreply.github.com>
2022-11-28 21:46:40 +0800

    Create jtool2.rb

---
+++ Casks/jtool2.rb
@@ -0,0 +1,12 @@
+cask "jtool2" do
+  # NOTE: "2" is not a version number, but an intrinsic part of the product name
+  version "2020.02.10"
+  sha256 :no_check
+
+  url "https://github.com/excitedplus1s/jtool2/releases/download/v2.1/jtool2.tgz"
+  name "jtool2"
+  desc "Tool to help out reverse engineering, security researchers, and tweak developers"
+  homepage "http://newosxbook.com/tools/jtool.html"
+
+  binary "jtool2"
+end
